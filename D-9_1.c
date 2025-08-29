2bc07f4978a1c8c4aea2a6852297d651119649d5
Devansh Aggarwal <214435331+devxnshx@users.noreply.github.com>
2025-08-29 17:34:04 +0530

    day 9

---
+++ D-9_1.c
@@ -0,0 +1,33 @@
+// Q17: Write a program to find the roots of a quadratic equation and categorize them.
+
+/*
+Sample Test Cases:
+Input 1:
+1 -3 2
+Output 1:
+Roots are real and different: 2, 1
+
+Input 2:
+1 -2 1
+Output 2:
+Roots are real and same: 1
+
+Input 3:
+1 2 5
+Output 3:
+Roots are complex
+
+*/
+
+#include <stdio.h>
+
+int main() {
+    /*quadratic equation => ax^2+bx+c*/
+
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+
+    int r1=(-b)+
+    
+     return 0;
+}
